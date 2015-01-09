d3459d2054014f0e5d7ed626684824f4857ad586
lab1b - Aaron Messner -Alex Lanz <csam5944@zid-t9pc1539.uibk.ac.at>
2015-01-09 09:45:16 +0100

    wordIterater

--- c++/Project/DicTree.cpp
+++ c++/Project/DicTree.cpp
@@ -26,13 +26,54 @@
               for (std::vector<int>::iterator inte=it->second.wordPosition.begin(); inte!=it->second.wordPosition.end(); ++inte){
 	 std::cout << *inte<<",";
               }
-               std::cout<<std::endl ;
+               //std::cout<<std::endl ;
             }else{
               std::cout << it->first<<std::endl ;
             }
               it->second.iterate();
          }
-          std::cout << ")" ;
+          std::cout << ")"<<std::endl ;
+     }   
+     
+      
+     std::vector<std::string> DicTree::wordIterate(){
+       std::vector<std::string> wordList;
+       std::string word;
+         for (std::map<char,DicTree>::iterator it=nodes.begin(); it!=nodes.end(); ++it){
+              std::vector<std::string> tmpList=it->second.secwordIterate();
+              for(std::vector<std::string>::iterator tmpit=tmpList.begin(); tmpit!=tmpList.end(); ++tmpit){
+	   wordList.push_back(it->first+*tmpit);
+	          std::cout<<"'"<<it->first+*tmpit<<"'"<<std::endl;
+         }
+         }
+           for(std::vector<std::string>::iterator w=wordList.begin(); w!=wordList.end(); ++w){
+	     std::cout<< *w<<std::endl;
+              }
+          return wordList;
+     }   
+     
+     
+       std::vector<std::string> DicTree::secwordIterate(){
+        std::vector<std::string> wordList;
+        std::string word;
+         for (std::map<char,DicTree>::iterator it=nodes.begin(); it!=nodes.end(); ++it){
+           //std::cout<<"'"<<it->first<<"'"<<it->second.nodes.size()<<"->"<<it->second.wordPosition.size()<<std::endl;
+            if(it->second.nodes.size()==0){
+              std::cout<<"a"<<std::endl;
+              std::vector<std::string> tmpList=it->second.wordIterate();
+              for(std::vector<std::string>::iterator tmpit=tmpList.begin(); tmpit!=tmpList.end(); ++tmpit){
+	   wordList.push_back(it->first+*tmpit);
+	          std::cout<<"'"<<it->first+*tmpit<<"'"<<std::endl;
+              }
+            }else{
+               std::cout<<"b"<<std::endl;
+              return std::vector<std::string> {it->first};
+            }
+         }
+           /*for(std::vector<std::string>::iterator w=wordList.begin(); w!=wordList.end(); ++w){
+	     std::cout<< *w<<std::endl;
+              }*/
+          return wordList;
      }   
      
      
